d0c01313703db73844228119540f3e4304ee0d5a
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-09 15:36:17 -0700

    imported objects into root html

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 // import functions and grab DOM elements
-
+import { signs } from '/data.js';
+import { renderSign } from './utils.js';
 // set event listeners 
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
 
 console.log('hello from app.js...');
+
+//let state
+const signListEl = document.getElementById('signs');
+
+for (let sign of signs) {
+    const signDiv = renderSign(sign);
+    signListEl.append(signDiv);
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,5 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderSign } from '../utils.js';
+
+import { signs } from '/data.js';
 
 const test = QUnit.test;
 
@@ -15,4 +18,13 @@ test('time to test a function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('renderSign() function', (expect) => {
+
+    const expected = `<div class="sign"><h2>sagittarius</h2><h3>(nov.21 - dec.21)</h3><img src="./assets/sagittarius.jpg"><h4 class="saying">"seeking, seeking, seeking"</h4><p>sagittarius is a sun sign in the 9th house associated with:</p><ul><li>higher education</li><li>foreign travel</li><li>spirituality</li><li>wisdom</li></ul></div>`;
+
+    const actual = renderSign(signs[0]);
+
+    expect.equal(actual.outerHTML, expected);
 });

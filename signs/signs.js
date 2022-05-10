e43a8c1ea6a6379bfdfc9b69b757a4f54b7c5afc
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-09 17:30:08 -0700

    added most of signs html elements by signs js

--- signs/signs.js
+++ signs/signs.js
@@ -1,9 +1,24 @@
 import { findById } from '../utils.js';
 import { signs } from '../data.js';
 
-console.log('hello from signs.js');
+const signName = document.getElementById('sign-name');
+const signDate = document.getElementById('sign-date');
+const signImage = document.getElementById('sign-image');
+const signSaying = document.getElementById('sign-saying');
+const signHouse = document.getElementById('sign-house');
+const signTraits = document.getElementById('sign-traits');
+
+//console.log('hello from signs.js');
+//console.log(signName);
 
 const params = new URLSearchParams(window.location.search);
-console.log(params.get('id'), signs);
+//console.log(params.get('id'), signs);
 
-const signName = findById(params.get('id'), signs);
+const find = findById(params.get('id'), signs);
+
+signName.textContent = find.name;
+signImage.src = `../assets/${find.name}.jpg`;
+signDate.textContent = find.date;
+signSaying.textContent = `"${find.saying}"`;
+signHouse.textContent = `${find.name} is a ${find.type} sign in the ${find.house} house associated with:`;
+signTraits.textContent = `${find.traits}`;
